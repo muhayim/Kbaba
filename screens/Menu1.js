ced999b6ebde053e994ccdafbbb9be8161e9931c
muhayim <muhayimshareef015@gmail.com>
2024-01-06 21:31:28 +0530

    chore:update project files

--- screens/Menu1.js
+++ screens/Menu1.js
@@ -31,7 +31,9 @@ const Menu1 = ({ navigation }) => {
        Car  Safe
       </Text>
       <Text>
-        
+      Empower your journey with the convenience of on-demand car services, {'\n'}
+      bringing the expertise of a mobile car mechanic directly to your doorstep, {'\n'}
+      ensuring your road adventures stay smooth and worry-free.
       </Text>
 
       <TouchableOpacity
@@ -46,6 +48,11 @@ const Menu1 = ({ navigation }) => {
       />
 
       </TouchableOpacity>
+<Text>Wheel Safe</Text>
+<Text>Seamless journeys await with our app's on-road services—experience {'\n'}
+the ease of mobile tire care and expert assistance for a worry-free drive.{'\n'}
+ Your travels, our priority, right at your fingertips.</Text>
+
     </View>
  );
 };
