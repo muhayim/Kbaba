914b253981643df0641534f91b37ec60d5c8074e
muhayim <muhayimshareef015@gmail.com>
2024-03-07 22:21:09 +0530

    changed the colors

--- screens/Menyu1.js
+++ screens/Menyu1.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
@@ -6,11 +6,28 @@ import {
   TouchableOpacity,
   FlatList,
   SafeAreaView,
+  Pressable,
 } from "react-native";
 import { Button, Image } from "react-native-elements";
+import * as Font from "expo-font";
+import { Title } from "react-native-paper";
+import { color } from "react-native-elements/dist/helpers";
 
 const Menyu1 = ({ navigation }) => {
   const Nav = () => navigation.navigate("AllServices");
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        "Lato-Black": require("../assets/fonts/Lato-Black.ttf"),
+      });
+      setFontLoaded(true);
+    }
+
+    loadFont();
+  }, []);
 
   const data = [
     {
@@ -56,7 +73,7 @@ const Menyu1 = ({ navigation }) => {
       <View style={styles.itemContainer}>
         <TouchableOpacity onPress={() => navigation.navigate(item.source)}>
           <Image source={item.imageSource} style={styles.image} />
-          <Text style={styles.itemLabel}>{item.label}</Text>
+          <Text style={[styles.itemLabel, { fontFamily: "Lato-Black" }]}>{item.label}</Text>
         </TouchableOpacity>
       </View>
     );
@@ -66,31 +83,54 @@ const Menyu1 = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.buttonContainer}>
-   
-  </View>
+      {fontLoaded && (
+        <View style={styles.buttonContainer}>
+          <Text style={{ color: "white", fontFamily: "Lato-Black" ,fontSize: 25 }}>
+            RoadSavr
+          </Text>
+        </View>
+      )}
       <SafeAreaView style={styles.card}>
-        <Text>Your Location</Text>
-        <Text>(GetLocation)</Text>
+        {fontLoaded && (
+          <>
+            <Text style={{ fontFamily: "Lato-Black" }}>Your Location</Text>
+            <Text style={{ fontFamily: "Lato-Black" }}>(GetLocation)</Text>
+          </>
+        )}
       </SafeAreaView>
       <SafeAreaView style={styles.card}>
-        <Text>Today's Fuel Price :</Text>
-        <Text>(Get_fuel_Price)</Text>
+        {fontLoaded && (
+          <>
+            <Text style={{ fontFamily: "Lato-Black" }}>Space for Ads</Text>
+            
+          </>
+        )}
       </SafeAreaView>
-      <View style={styles.bottomHalf}>
+
+      <View style={[styles.bottomHalf, { fontFamily: "Lato-Black" }]}>
         <FlatList
           data={data}
           renderItem={renderItem}
           keyExtractor={keyExtractor}
           numColumns={3}
+          
         />
-        <Button
-          title="All Services"
-          onPress={Nav}
-          style={styles.Btn}
-          color={"#cadcfc"}
-        />
+        
       </View>
+       <View>
+       {fontLoaded &&(
+        <Pressable
+        
+        onPress={() =>navigation.navigate("AllServices")}
+        style={({ pressed }) => [
+          styles.bottomHalf,
+          { opacity: pressed ? 0.5 : 1 } , {marginTop : 30}  
+        ]}
+      >
+        <Text style={[styles.title , {fontFamily : "Lato-Black"}, {color:"white"}]} >Garage</Text>
+      </Pressable>
+      )} 
+      </View> 
     </SafeAreaView>
   );
 };
@@ -98,24 +138,27 @@ const Menyu1 = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#f5eea9",
+    fontFamily:"Lato-Black"
   },
   bottomHalf: {
-    backgroundColor: "black",
+    backgroundColor: "#549d5f",
     borderRadius: 30,
-    flex: 1,
+    flex: 0,
     justifyContent: "center",
     alignItems: "center",
-    padding: 30,
+    padding: 20,
+    fontFamily:"Lato-Black"
   },
   itemContainer: {
     alignItems: "center",
     margin: 10,
     width: 75,
+    fontFamily:"Lato-Black"
   },
   image: {
-    width: 80,
-    height: 80,
+    width: 52,
+    height: 52,
     borderRadius: 50,
     borderWidth: 1,
     borderColor: "white",
@@ -126,33 +169,39 @@ const styles = StyleSheet.create({
     marginTop: 1,
     textAlign: "center",
     color: "white",
+    fontFamily: "Lato-Black",
   },
   Btn: {
     width: "80%",
     flex: 1,
     alignItems: "center",
+    fontFamily: "Lato-Black",
   },
   card: {
     backgroundColor: "white",
     borderRadius: 8,
     paddingVertical: 45,
     paddingHorizontal: 25,
+    paddingTop: 300,
     width: "100%",
     marginVertical: 10,
     shadowColor: "gray",
     shadowOffset: { width: -2, height: 4 },
     shadowOpacity: 0.5,
     shadowRadius: 3,
-    elevation: 5, // This is for Android
+    elevation: 5,
+    paddingTop: -1
   },
   buttonContainer: {
     backgroundColor: "black",
-    padding:70, // Adjust padding as needed
-    flexDirection: "row", // Adjust flexDirection as needed
-    justifyContent: "space-around", // Adjust justifyContent as needed
-    alignItems: "center", // Adjust alignItems as needed
+    padding: 30,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    fontFamily:"Lato-Black",
+    borderRadius: 33,
+    backgroundColor: "#549d5f"
   },
-  
 });
 
 export { Menyu1 };
